6a1a0813ea57862d5d37d3c0d1972c6d501f22c4
Giovanni Fioretto <giovanni.fioretto.23@gmail.com>
2024-11-04 10:37:45 +0100

    Initial Commit

---
+++ AlmondTeamtest/AlmondTeamtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmondTeamtestApp.swift
+//  AlmondTeamtest
+//
+//  Created by Giovanni Fioretto on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AlmondTeamtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
